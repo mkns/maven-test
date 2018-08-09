7ff268f614dcd6679f1d66f977aaa9a31d6957fa
Kenny Scott <mkns@oxygen.home>
2018-08-09 23:34:42 +0100

    Initial commit

---
+++ src/main/java/com/kennyscott/maventest/Foo.java
@@ -0,0 +1,19 @@
+package com.kennyscott.maventest;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Foo {
+
+	public String foo;
+	public String bar = "yak yak";
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+
+}
---
+++ src/main/java/com/kennyscott/maventest/controller/MyController.java
@@ -0,0 +1,38 @@
+package com.kennyscott.maventest.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.kennyscott.maventest.Foo;
+
+@Controller
+public class MyController {
+
+	@Autowired
+	private Foo foo;
+	
+	@Autowired
+	private Gson gson;
+
+	final static Logger LOG = Logger.getLogger(MyController.class);
+
+	@RequestMapping(value = "/hey", method = RequestMethod.GET)
+	public String handle() {
+		LOG.warn("Some random warning");
+		return "/resources/html/hello.html";
+	}
+
+	@RequestMapping(value = "/heyJson", method = RequestMethod.GET)
+	@ResponseBody
+	public String fnarr() {
+		foo.setFoo("fnarr");
+		return gson.toJson(foo);
+	}
+
+}
